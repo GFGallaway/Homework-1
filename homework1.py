5af70a4ce5b576ff497f1e3f8ab5d79ba269ab53
Gabby F G <123026505+GFGallaway@users.noreply.github.com>
2023-02-05 16:18:09 -0500

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,27 @@
+# author is Gabby Gallaway
+#Problem 1
+print("Rosewater")
+print("0.001")
+30+7
+bool(0)
+#Problem 2
+#For the +, it gives the sum right away when run.
+1+1
+#For a comma, it needs to have a value assigned to a digit.
+y=2
+print("The sum of 1+1 is", y)
+#Problem 3
+float(3)
+int(2.5)
+int(False)
+bool(1)
+#Problem 4
+print("To display a multi-line paragraph,\nyou need to use a backwards slash and an n.\nDepending on how many you put in your code,\nit will create a brand new line after each slash-n.\nI hope this looks right.")
+#Problem 5
+flour=2
+milk=1
+egg=4  
+oil=0.4 
+vanilla=0.012
+cake=2*flour+0.5*(milk-egg+vanilla)+oil**2
+print(cake)
